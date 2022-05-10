a0d9e78079c0442fd8032c4374242cd66ca9008a
khushi8686 <khushi.muhammad8686@gmail.com>
2022-05-10 11:07:10 +0500

    enter my name in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,96 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  var image;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: Center(
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('image/khushi.jpg'),
+                ),
+                Text(
+                  'Khushi Muhammad',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  //khushi
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.teal.shade100,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 4),
+                ),
+                SizedBox(
+                  height: 20,
+                  width: 200,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 25,
+                  ),
+                      child: ListTile(
+                        leading: Icon(Icons.call,
+                          color: Colors.teal,
+                        ),
+                        title:Text(
+                          '+92 309 7728004',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Source Sans Pro',
+                            fontSize: 17,
+                          ),
+                        ),
+                      ),
+                  ),
+                Card(
+                  margin: EdgeInsets.symmetric(vertical: 10,
+                  horizontal: 25,
+                  ),
+                  child: ListTile(
+                  leading: Icon(Icons.mail,
+                    color: Colors.teal,
+                  ),
+                    title:Text(
+                      'Khushi.Muhammad8686@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
